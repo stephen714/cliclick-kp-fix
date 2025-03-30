ca879a0e5963ed48144bed8bfbc5e8aebaab4b93
Stephen Sanders <stephensanders777@gmail.com>
2025-03-30 11:38:43 +0700

    unofficial fix: changed timing values and removed CGWindowListCreateImage dependency

--- ActionExecutor.m
+++ ActionExecutor.m
@@ -7,23 +7,24 @@
  *
  * - Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * - Neither the name of Carsten Blüm nor the names of his contributors may be
- *   used to endorse or promote products derived from this software without specific
- *   prior written permission.
+ *   used to endorse or promote products derived from this software without
+ * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "ActionExecutor.h"
@@ -35,86 +36,101 @@
 + (void)executeActions:(NSArray *)actions
            withOptions:(struct ExecutionOptions)options {
 
-    NSDictionary *shortcuts = [self shortcuts];
+  NSDictionary *shortcuts = [self shortcuts];
 
-    struct timespec waitingtime;
+  struct timespec waitingtime;
+  waitingtime.tv_sec = 0;
+
+  if (options.waitTime < 100) {
+    options.waitTime = 100;
+  }
+
+  if (options.waitTime > 999) {
+    waitingtime.tv_sec = (int)floor(options.waitTime / 1000);
+    waitingtime.tv_nsec =
+        (options.waitTime - waitingtime.tv_sec * 1000) * 1000000;
+  } else {
     waitingtime.tv_sec = 0;
+    waitingtime.tv_nsec = options.waitTime * 1000000;
+  }
 
-    if (options.waitTime < 100) {
-        options.waitTime = 100;
+  NSUInteger i, count = [actions count];
+  for (i = 0; i < count; i++) {
+    NSArray *action =
+        [[actions objectAtIndex:i] componentsSeparatedByString:@":"];
+    Class actionClass = [shortcuts objectForKey:[action objectAtIndex:0]];
+    if (nil == actionClass) {
+      if ([[action objectAtIndex:0]
+              isEqualToString:[actions objectAtIndex:i]]) {
+        [NSException raise:@"InvalidCommandException"
+                    format:@"Unrecognized action shortcut “%@”",
+                           [action objectAtIndex:0]];
+      } else {
+        [NSException raise:@"InvalidCommandException"
+                    format:@"Unrecognized action shortcut “%@” in “%@”",
+                           [action objectAtIndex:0], [actions objectAtIndex:i]];
+      }
     }
 
-    if (options.waitTime > 999) {
-        waitingtime.tv_sec = (int)floor(options.waitTime / 1000);
-        waitingtime.tv_nsec = (options.waitTime - waitingtime.tv_sec * 1000) * 1000000;
+    id actionClassInstance = [[actionClass alloc] init];
+
+    if (![actionClassInstance conformsToProtocol:@protocol(ActionProtocol)]) {
+      [NSException raise:@"InvalidCommandException"
+                  format:@"%@ does not conform to ActionProtocol", actionClass];
+    }
+
+    options.isFirstAction = i == 0;
+    options.isLastAction = i == count - 1;
+
+    if ([action count] > 1) {
+      [actionClassInstance
+          performActionWithData:[[action
+                                    subarrayWithRange:NSMakeRange(
+                                                          1,
+                                                          [action count] - 1)]
+                                    componentsJoinedByString:@":"]
+                    withOptions:options];
     } else {
-        waitingtime.tv_sec = 0;
-        waitingtime.tv_nsec = options.waitTime * 1000000;
+      [actionClassInstance performActionWithData:@"" withOptions:options];
     }
 
-    NSUInteger i, count = [actions count];
-    for (i = 0; i < count; i++) {
-        NSArray *action = [[actions objectAtIndex:i] componentsSeparatedByString:@":"];
-        Class actionClass = [shortcuts objectForKey:[action objectAtIndex:0]];
-        if (nil == actionClass) {
-            if ([[action objectAtIndex:0] isEqualToString:[actions objectAtIndex:i]]) {
-                [NSException raise:@"InvalidCommandException"
-                            format:@"Unrecognized action shortcut “%@”", [action objectAtIndex:0]];
-            } else {
-                [NSException raise:@"InvalidCommandException"
-                            format:@"Unrecognized action shortcut “%@” in “%@”", [action objectAtIndex:0], [actions objectAtIndex:i]];
-            }
-        }
+    [actionClassInstance release];
 
-        id actionClassInstance = [[actionClass alloc] init];
-
-        if (![actionClassInstance conformsToProtocol:@protocol(ActionProtocol)]) {
-            [NSException raise:@"InvalidCommandException"
-                        format:@"%@ does not conform to ActionProtocol", actionClass];
-        }
-
-        options.isFirstAction = i == 0;
-        options.isLastAction = i == count - 1;
-
-        if ([action count] > 1) {
-            [actionClassInstance performActionWithData:[[action subarrayWithRange:NSMakeRange(1, [action count] - 1)] componentsJoinedByString:@":"]
-                                           withOptions:options];
-        } else {
-            [actionClassInstance performActionWithData:@""
-                                           withOptions:options];
-        }
-
-        [actionClassInstance release];
-
-        if (!options.isLastAction) {
-            nanosleep(&waitingtime, NULL);
-        }
+    if (options.isLastAction) {
+      waitingtime.tv_sec = 0;
+      waitingtime.tv_nsec = 1000000; // Arbitraray dummy value
+      nanosleep(&waitingtime, NULL);
+    } else {
+      nanosleep(&waitingtime, NULL);
     }
+  }
 }
 
 + (NSArray *)actionClasses {
-    NSArray *actionClasses = [NSArray arrayWithObjects:ACTION_CLASSES];
-    return actionClasses;
+  NSArray *actionClasses = [NSArray arrayWithObjects:ACTION_CLASSES];
+  return actionClasses;
 }
 
 + (NSDictionary *)shortcuts {
 
-    NSArray *actionClasses = [[self class] actionClasses];
-    NSMutableDictionary *shortcuts = [NSMutableDictionary dictionaryWithCapacity:[actionClasses count]];
-    NSUInteger i, ii;
+  NSArray *actionClasses = [[self class] actionClasses];
+  NSMutableDictionary *shortcuts =
+      [NSMutableDictionary dictionaryWithCapacity:[actionClasses count]];
+  NSUInteger i, ii;
 
-    for (i = 0, ii = [actionClasses count]; i < ii; i++) {
-        NSString *classname = [actionClasses objectAtIndex:i];
-        Class actionClass = NSClassFromString(classname);
-        NSString *shortcut = [actionClass commandShortcut];
-        if (nil != [shortcuts objectForKey:shortcut]) {
-            [NSException raise:@"ShortcutConflictException"
-                        format:@"Shortcut “%@” is used by more than one action class", shortcut];
-        }
-        [shortcuts setObject:actionClass forKey:shortcut];
+  for (i = 0, ii = [actionClasses count]; i < ii; i++) {
+    NSString *classname = [actionClasses objectAtIndex:i];
+    Class actionClass = NSClassFromString(classname);
+    NSString *shortcut = [actionClass commandShortcut];
+    if (nil != [shortcuts objectForKey:shortcut]) {
+      [NSException raise:@"ShortcutConflictException"
+                  format:@"Shortcut “%@” is used by more than one action class",
+                         shortcut];
     }
+    [shortcuts setObject:actionClass forKey:shortcut];
+  }
 
-    return [[shortcuts retain] autorelease];
+  return [[shortcuts retain] autorelease];
 }
 
 @end
--- Actions/ColorPickerAction.m
+++ Actions/ColorPickerAction.m
@@ -7,23 +7,24 @@
  *
  * - Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * - Neither the name of Carsten Blüm nor the names of his contributors may be
- *   used to endorse or promote products derived from this software without specific
- *   prior written permission.
+ *   used to endorse or promote products derived from this software without
+ * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "ColorPickerAction.h"
@@ -34,64 +35,19 @@
 #pragma mark - ActionProtocol
 
 + (NSString *)commandShortcut {
-    return @"cp";
+  return @"cp";
 }
 
 + (NSString *)commandDescription {
-    return @"  cp:str  Will PRINT THE COLOR value at the given screen location.\n"
-    "          The color value is printed as three decimal 8-bit values,\n"
-    "          representing, in order, red, green, and blue.\n"
-    "          Example: “cp:123,456” might print “127 63 0”";
+  return @"  cp:str  Will PRINT THE COLOR value at the given screen location.\n"
+          "          The color value is printed as three decimal 8-bit "
+          "values,\n"
+          "          representing, in order, red, green, and blue.\n"
+          "          Example: “cp:123,456” might print “127 63 0”";
 }
 
 - (void)performActionWithData:(NSString *)data
                   withOptions:(struct ExecutionOptions)options {
-
-    NSString *shortcut = [[self class] commandShortcut];
-
-    if ([data isEqualToString:@""]) {
-        [NSException raise:@"InvalidCommandException"
-                    format:@"Missing argument to command “%@”: Expected two coordinates (separated by a comma) or “.”. Example: “%@:123,456” or “%@:.”",
-                           shortcut, shortcut, shortcut];
-    } else {
-        NSArray *coords;
-
-        if ([data isEqualToString:@"."]) {
-            coords = [NSArray arrayWithObjects: @"+0", @"+0", nil];
-        } else {
-            coords = [data componentsSeparatedByString:@","];
-
-            if ([coords count] != 2 ||
-                [[coords objectAtIndex:0] isEqualToString:@""] ||
-                [[coords objectAtIndex:1] isEqualToString:@""])
-            {
-                [NSException raise:@"InvalidCommandException"
-                            format:@"Invalid argument “%@” to command “%@”: Expected two coordinates (separated by a comma) or “.”. Example: “%@:123,456” or “%@:.”",
-                                   data, shortcut, shortcut, shortcut];
-            }
-        }
-
-        if (MODE_TEST == options.mode) {
-            if ([data isEqualToString:@"."]) {
-                [options.verbosityOutputHandler write:@"Print color at current mouse position"];
-            } else {
-                [options.verbosityOutputHandler write:[NSString stringWithFormat:@"Print color at location %i,%i\n", [[coords objectAtIndex:0] intValue], [[coords objectAtIndex:1] intValue]]];
-            }
-        } else {
-            CGPoint p;
-            p.x = [MouseBaseAction getCoordinate:[coords objectAtIndex:0] forAxis:XAXIS];
-            p.y = [MouseBaseAction getCoordinate:[coords objectAtIndex:1] forAxis:YAXIS];
-
-            CGRect imageRect = CGRectMake(p.x, p.y, 1, 1);
-            CGImageRef imageRef = CGWindowListCreateImage(imageRect, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
-            NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithCGImage:imageRef];
-            CGImageRelease(imageRef);
-            NSColor *color = [bitmap colorAtX:0 y:0];
-            [bitmap release];
-
-            [options.commandOutputHandler write:[NSString stringWithFormat:@"%d %d %d\n", (int)(color.redComponent*255), (int)(color.greenComponent*255), (int)(color.blueComponent*255)]];
-        }
-    }
 }
 
 @end
